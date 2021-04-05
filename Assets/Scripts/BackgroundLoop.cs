b018332a6ae6c9e1c91995d7bf750e2f5e43e00d
ldh2003 <80668146+ldh2003@users.noreply.github.com>
2021-04-05 15:10:33 +0900

    0405

---
+++ Assets/Scripts/BackgroundLoop.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundLoop : MonoBehaviour
+{
+    private class BackgroundLoop : MonoBehaviour
+        private
+    void Update()
+    {
+        
+    }
+}
