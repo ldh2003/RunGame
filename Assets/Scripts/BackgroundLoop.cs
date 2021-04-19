371ce53ad6f6269ddb4d3eef9d9d8359a475807f
ldh2003 <80668146+ldh2003@users.noreply.github.com>
2021-04-19 15:19:01 +0900

    0419

--- Assets/Scripts/BackgroundLoop.cs
+++ Assets/Scripts/BackgroundLoop.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class BackgroundLoop : MonoBehaviour
 {
-    private class BackgroundLoop : MonoBehaviour
-        private
+    private float width;
+    private void Awake()
+    {
+        BoxCollider2D backgroundcollider = GetComponent<BoxCollider2D>();
+        width = backgroundcollider.size.x;
+    }
     void Update()
     {
-        
+        if(transform.position.x <= -width)
+        {
+            Reposition();
+        }
+    }
+    private void Reposition()
+    {
+        Vector2 offset = new Vector2(width * 2f, 0);
+        transform.position = (Vector2)transform.position + offset;
     }
 }
